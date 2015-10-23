711900efaa8545cd9b7f53ca568e887addb2fba0
Justin Grant <jlgrock@gmail.com>
2015-10-23 13:59:26 -0400

    refactor

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-# Load the version from the VERSION file
-for line in $(< VERSION)
-do
-  case $line in
-    CENTOS=*)  eval $line ;; # beware! eval!
-	JDK=*)  eval $line ;; # beware! eval!
-    *) ;;
-   esac
-done
+# load the versions
+/bin/sh ./loadenv.sh
+
 echo "Processing for CentOS + JDK - Version $CENTOS-$JDK" 
 
 # Build the image
--- publish.sh
+++ publish.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# Load the version from the VERSION file
-for line in $(< VERSION)
-do
-  case $line in
-    CENTOS=*)  eval $line ;; # beware! eval!
-	JDK=*)  eval $line ;; # beware! eval!
-    *) ;;
-   esac
-done
+# load the versions
+/bin/sh ./loadenv.sh
 
+# Publish the image
 /bin/sh ./build.sh
 
 # Build the image
