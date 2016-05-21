7a0fa880a681acdc8768b3aae2f24ee7120fb492
Justin Grant <jlgrock@gmail.com>
2016-05-21 16:49:05 -0400

    minor modifications to output/docs

--- build.sh
+++ build.sh
@@ -21,7 +21,13 @@ ID=$(docker run -d ${TMP_IMAGE_NAME}:${IMAGE_VERSION} /bin/bash)
 echo "Container ID '$ID' now running"
 
 # Flatten the image (removes AUFS layers) and create a new image
+# Note that you lose history/layers and environment variables with this method,
+# but it slims down the image significantly.  The known necessary environment
+# variables have been added back
 docker export ${ID} | docker import - ${IMAGE_NAME}:${IMAGE_VERSION}
+echo "Created Flattened image with ID: ${FLAT_ID} for ${IMAGE_NAME}:${IMAGE_VERSION}"
+
+# Cleanup
 echo "destroying images/containers related to $TMP_IMAGE_NAME (all versions)"
 docker ps -a | awk '{ print $1,$2 }' | grep ${TMP_IMAGE_NAME} | awk '{ print $1 }' | xargs -I {} docker rm {}
 docker images -a | awk '{ print $1, $3 }' | grep ${TMP_IMAGE_NAME} | awk '{ print $2 }' | xargs -I {} docker rmi {}
